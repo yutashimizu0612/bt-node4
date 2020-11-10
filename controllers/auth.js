8a31d4b7b179bc191193542c8e4a2ca8a59d54da
yutashimizu0612 <s08388yskh@gmail.com>
2020-11-10 19:32:30 +0900

    パスワードのハッシュ化とname&emailの重複時のエラー表示

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const connection = require('../model/dbConnection');
 const { validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
 
 exports.register = (req, res) => {
   // バリデーションエラーの場合、エラー文と入力値を渡す
@@ -8,13 +9,23 @@ exports.register = (req, res) => {
     return res.render('pages/register', { errors: errors.array() });
   }
   // ユーザ登録
+  const hashedPassword = bcrypt.hashSync(req.body.password, 10);
   const newUser = {
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   };
-  connection.query('INSERT INTO users SET ?', newUser, function (err, result) {
-    if (err) throw err;
+  connection.query('INSERT INTO users SET ?', newUser, function (
+    error,
+    result
+  ) {
+    if (error) {
+      console.log(error);
+      return res.render('pages/register', {
+        errors: [{ msg: '名前かメールアドレスが重複しています。' }],
+      });
+    }
     console.log('new user is created');
+    res.redirect('/');
   });
 };
