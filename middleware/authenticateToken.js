ce3f3fb8b4c1e6930546aa1bbd6349a1b9ffd3e0
yutashimizu0612 <s08388yskh@gmail.com>
2020-11-15 13:37:08 +0900

    authenticateTokenの実装

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -4,10 +4,11 @@ const jwt = require('jsonwebtoken');
 exports.authenticateToken = (req, res, next) => {
   const authHeader = req.headers['authorization'];
   const token = authHeader && authHeader.split(' ')[1];
-  if (token === null) return res.sendStatus(401);
+  if (!token) return res.sendStatus(401);
 
-  jwt.verify(token, process.env, (error, user) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, user) => {
     if (error) return res.sendStatus(403);
+    console.log('user', user);
     req.user = user;
     next();
   });
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
 
 const auth = require('./controllers/auth');
-const validation = require('./functions/validation');
+const validation = require('./middleware/validation');
+const { authenticateToken } = require('./middleware/authenticateToken');
 
 const app = express();
 
@@ -12,8 +12,9 @@ require('dotenv').config();
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/static', express.static(__dirname + '/static'));
 
-app.get('/', (req, res) => {
+app.get('/', authenticateToken, (req, res) => {
   res.render('pages/index');
 });
 
@@ -31,6 +32,10 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
   auth.login(req, res);
+});
+
+app.post('/token', (req, res) => {
+  const refreshToken = req.body.token;
 });
 
 app.listen(3000, () => {
