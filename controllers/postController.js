55b87bb7e6f5737e2a2f926369611470be850488
yutashimizu0612 <s08388yskh@gmail.com>
2020-11-18 22:31:27 +0900

    不要ログ削除

--- controllers/postController.js
+++ controllers/postController.js
@@ -3,19 +3,14 @@ const Post = require('../model/Post');
 
 module.exports = {
   doGetAllPosts: async (req, res) => {
-    console.log('doGetAllPosts');
     const posts = await Post.getAllPosts();
     res.render('pages/posts', { posts });
   },
   showEditPage: async (req, res) => {
-    console.log('showEditPage');
-    // idに基準に、該当のpostデータを1件取得
     const post = await Post.getPost(req.params.id);
-    console.log('post at showEditPage', post);
     res.render('pages/edit', { post });
   },
   doCreateNewPost: async (req, res) => {
-    console.log('doCreateNewPost');
     await Post.createNewPost(req.body.title, req.body.content);
     res.redirect('/post');
   },
