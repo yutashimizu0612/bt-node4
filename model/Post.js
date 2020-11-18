6fb0c2994fffff6d34b712d030ec031ce9e9fe79
yutashimizu0612 <s08388yskh@gmail.com>
2020-11-18 22:28:58 +0900

    model内の新規投稿処理を簡潔にする

--- model/Post.js
+++ model/Post.js
@@ -30,21 +30,18 @@ module.exports = {
   },
   // TODO 投稿ユーザのidも追加する必要あり
   createNewPost: async (title, content) => {
-    let connection;
     try {
-      connection = await mysql.createConnection(db_setting);
+      const connection = await mysql.createConnection(db_setting);
       await connection.execute(
         `INSERT INTO ${table} SET title = ?, content = ?`,
         [title, content]
       );
       console.log('new post is created');
+      console.log('DBconnection is closed');
+      await connection.end();
     } catch (error) {
       console.log('error', error);
       return res.status(400).json({ error: error });
-    } finally {
-      console.log('DBconnection is closed');
-      await connection.end();
-      return;
     }
   },
   updatePost: () => {},
