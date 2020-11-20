a606e01e22d31daca93b6a59c0fa69a169a1be43
yutashimizu0612 <s08388yskh@gmail.com>
2020-11-20 22:54:30 +0900

    format

--- static/js/index.js
+++ static/js/index.js
@@ -1,23 +1,23 @@
-const handleLoginSubmit = data => {
-  fetch('http://localhost:3000/login', {
+const handleLoginSubmit = (data) => {
+  fetch('http://localhost:3000/auth/login', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(data),
   })
-    .then(response => response.json())
-    .then(result => {
+    .then((response) => response.json())
+    .then((result) => {
       if (!result.error) {
         localStorage.setItem('token', result.token);
       } else {
         console.log(result.error);
       }
     })
-    .catch(error => console.log(error));
+    .catch((error) => console.log(error));
 };
 
-document.getElementById('login').addEventListener('submit', event => {
+document.getElementById('login').addEventListener('submit', (event) => {
   event.preventDefault();
   const loginForm = document.forms['login'];
   const data = {
