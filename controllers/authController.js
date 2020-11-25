6f4585d360a9ee33561f37f3b1681ad0ccb39669
yutashimizu0612 <s08388yskh@gmail.com>
2020-11-25 20:36:19 +0900

    403ステータスコードを追加

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,9 +2,7 @@ const { validationResult } = require('express-validator');
 const bcrypt = require('bcrypt');
 
 const Auth = require('../models/Auth');
-const {
-  setAccessTokenToCookie,
-} = require('../functions/setAccessTokenToCookie');
+const { setAccessTokenToCookie } = require('../functions/setAccessTokenToCookie');
 
 require('dotenv').config();
 
@@ -40,13 +38,13 @@ module.exports = {
       const user = await Auth.getUserByEmail(req.body.email);
       // ユーザが見つからない場合
       if (!user) {
-        return res.json({ error: '登録されていないメールアドレスです。' });
+        return res.status(403).json({ error: '登録されていないメールアドレスです。' });
       }
       const match = await bcrypt.compare(req.body.password, user.password);
       if (match) {
         setAccessTokenToCookie(res, user.id);
       } else {
-        return res.json({ error: 'パスワードが間違っています。' });
+        return res.status(403).json({ error: 'パスワードが間違っています。' });
       }
     } catch (error) {
       console.log('error', error);
